58e4c1bcf901fb0f05db790c152484f5b4f11128
S. Arjun <arjunsenthilvel1@gmail.com>
2020-08-15 13:51:07 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Facebook from "./screens/fb";
+import Instagram from "./screens/ig";
+
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs.Navigator>
+        <Tabs.Screen name="Facebook" component={Facebook} />
+        <Tabs.Screen name="Instagram" component={Instagram} />
+      </Tabs.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/ig.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function Instagram() {
+  return <Text>Instagram</Text>;
+}
